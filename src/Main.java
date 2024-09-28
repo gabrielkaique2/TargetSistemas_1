66a02b3d6c1a71deb6c44024fba8239b4fb3ef86
Gabriel_Kaique <79485516+gabrielkaique2@users.noreply.github.com>
2024-09-27 21:49:50 -0300

    "1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;  Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }  Imprimir(SOMA);  Ao final do processamento, qual será o valor da variável SOMA?"

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+    int indice = 13;
+    int SOMA = 0;
+    int K = 0;
+
+        while(K<indice){
+            K = K+ 1;
+            SOMA =SOMA + K;
+    }
+        System.out.println(SOMA);
+    }
+}
